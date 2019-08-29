82244334f0e2697d710535ea1c3d71784f1692ee
Saket Choudhary <saketkc@gmail.com>
2019-08-28 20:51:29 -0700

    Fix type

--- init.py
+++ init.py
@@ -5,7 +5,7 @@ __DATASETS__ = pd.read_csv(__DATASET_PATH__, sep="\t").set_index("species")
 
 __SPECIES__ = sorted(
     [
-        {"label": "H.sapiensi (human)", "value": "hg38"},
+        {"label": "H.sapiens (human)", "value": "hg38"},
         {"label": "M.musculus (mouse)", "value": "mm10"},
         {"label": "C.albicans (candida albicans)", "value": "SC5314"},
         {"label": "D.melanogaster (housefly)", "value": "BDGP6"},
@@ -18,7 +18,7 @@ __SPECIES__ = sorted(
         {"label": "S.cerevisiae (yeast)", "value": "R64-1-1"},
         {"label": "A.thaliana (arabidopsis)", "value": "TAIR10"},
         {"label": "S.pombe (pombe)", "value": "ASM294v2"},
-        {"label": "E.colii (ecoli)", "value": "ASM584v2"},
+        {"label": "E.coli (ecoli)", "value": "ASM584v2"},
         {"label": "P.falciparum (plasmodium falciparum)", "value": "Epr1"},
         {"label": "C.griseus (hamster)", "value": "CriGri_1.0"},
     ],
